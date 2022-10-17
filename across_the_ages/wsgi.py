9b1fa4097905910fcb6f439ffc1f9f419911b931
Claire221 <c.biglands@hotmail.com>
2022-10-17 12:19:31 +0000

    Add initial project setup

---
+++ across_the_ages/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for across_the_ages project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'across_the_ages.settings')
+
+application = get_wsgi_application()
