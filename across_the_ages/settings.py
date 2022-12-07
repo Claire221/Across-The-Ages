a83a613e88e95ac82058a6bb065ddcc54bd8f595
Claire221 <c.biglands@hotmail.com>
2022-12-07 09:11:13 +0000

    Added cache control

--- across_the_ages/settings.py
+++ across_the_ages/settings.py
@@ -169,6 +169,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = "across-the-ages"
     AWS_S3_REGION_NAME = 'london'
     ASW_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
