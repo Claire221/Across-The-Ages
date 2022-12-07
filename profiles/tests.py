40635eace452391ed82d9b40ed553ba2e56c5fe8
Claire221 <c.biglands@hotmail.com>
2022-12-07 13:48:23 +0000

    Create user profiles

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-def profile(request):
-    template = 'profiles/profile.html'
-    context = {}
-
-    return render(request, template, context)
