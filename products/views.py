b5331c9dad06807c23e3fa0a32441d0af9a046f1
Claire221 <c.biglands@hotmail.com>
2023-01-17 09:12:37 +0000

    Add function to add/edit/delete products

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 import random
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Product, Category
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -81,3 +83,68 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+@login_required
+def add_product(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you are not authorised to preform this function')
+        return redirect(reverse('home'))
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product successfully added')
+            return redirect(reverse('product_detail', args[product.id]))
+        else:
+            messages.error(request, 'Failed to add product, please try again.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+    
+    return render(request, template, context)
+
+@login_required
+def edit_product(request, product_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you are not authorised to preform this function')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product successfully updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please try again')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+    
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+    
+    return render(request, template, context)
+
+@login_required
+def delete_product(request, product_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you are not authorised to preform this function')
+        return redirect(reverse('home'))
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product successfully deleted')
+
+    return redirect(reverse('products'))
