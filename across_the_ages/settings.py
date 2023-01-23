3efba08b53e76485a6ddbbbbba15ba6a5e9efebc
Claire221 <c.biglands@hotmail.com>
2023-01-23 09:08:20 +0000

    Add static files to heroky app

--- across_the_ages/settings.py
+++ across_the_ages/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'crispy_forms',
-    "storages"
+    "storages",
 ]
 
 MIDDLEWARE = [
