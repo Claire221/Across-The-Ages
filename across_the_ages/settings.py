239e2b7e9d37e134fc1d08705e0a65bcb78fc833
Claire221 <c.biglands@hotmail.com>
2023-01-23 09:50:32 +0000

    Fix type on settings.py

--- across_the_ages/settings.py
+++ across_the_ages/settings.py
@@ -183,9 +183,9 @@ if 'USE_AWS' in os.environ:
     ASW_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    STATICFILES_STORAGE = 'custome_storages.StaticStorage'
-    STATICFILES_LCCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custome_storages.MediaStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
