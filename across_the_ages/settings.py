9e7084154dc4f109161ae1d782d451f6364d8877
Claire221 <c.biglands@hotmail.com>
2023-01-23 11:49:10 +0000

    URemove white space

--- across_the_ages/settings.py
+++ across_the_ages/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    AWS_STORAGE_BUCKET_NAME  = "across-the-ages"
+    AWS_STORAGE_BUCKET_NAME = "across-the-ages"
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
