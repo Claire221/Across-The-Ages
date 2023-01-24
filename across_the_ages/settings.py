a949f1f8ecc29854b572ed77527bcb8307ce054c
Claire221 <c.biglands@hotmail.com>
2023-01-24 08:54:23 +0000

    Fix typo in settings.py

--- across_the_ages/settings.py
+++ across_the_ages/settings.py
@@ -198,19 +198,18 @@ FREE_DELIVERY_THRESHOLD = 50
 # STANDARD_DELIVERY_PERCENTAGE = 10
 STANDARD_DELIVERY = 10
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', 'pk_test_51M94xIHVGSxAtM8PTGUbFhFxZhojInqWSF8Jc7fIaEvholWwNXvqnufyFqM53uxge740d3idxHneOn4E3tk5LUEb00EJtMqpN7')
-STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', 'sk_test_51M94xIHVGSxAtM8PtdiAHHXmPfnweuxFhHoSOo4YM87X2Ox26BYeKVlmFFHhebR5TeLYr9PVQIuJcYu8BuNDAFeK00gcOwYYXR')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', 'whsec_rqIZtvL1Qu80GkQTKRW77AGysAdjg9DJ')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51M94xIHVGSxAtM8PTGUbFhFxZhojInqWSF8Jc7fIaEvholWwNXvqnufyFqM53uxge740d3idxHneOn4E3tk5LUEb00EJtMqpN7')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51M94xIHVGSxAtM8PtdiAHHXmPfnweuxFhHoSOo4YM87X2Ox26BYeKVlmFFHhebR5TeLYr9PVQIuJcYu8BuNDAFeK00gcOwYYXR')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_rqIZtvL1Qu80GkQTKRW77AGysAdjg9DJ')
 
-if 'DEVELOPMENT' in os.environ: 
-    EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'acrosstheages@hotmail.com'
-else: 
-    EMAIL_BACKEND ='django.core.mail.backends.sntp.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'sntp.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
