f5c4f98ebd96ee4538dd4bdcbee188351650d50c
Claire221 <c.biglands@hotmail.com>
2023-02-07 14:42:01 +0000

    Remove unused field

--- products/models.py
+++ products/models.py
@@ -22,8 +22,6 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    #remove if unused
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
