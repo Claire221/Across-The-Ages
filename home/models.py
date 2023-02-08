eff75a566dddd7bcb3b005def1dd267c27c41bac
Claire221 <c.biglands@hotmail.com>
2023-02-08 13:49:23 +0000

    Add code to stop users submiting blank forms

--- home/models.py
+++ home/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 class Contact(models.Model):
-    first_name = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=20, null=True, blank=False)
     last_name = models.CharField(max_length=20, null=True, blank=True)
-    email = models.CharField(max_length=30, null=True, blank=True)
-    message = models.TextField(null=True, blank=True)
+    email = models.CharField(max_length=30, null=True, blank=False)
+    message = models.TextField(null=True, blank=False)
 
     def __str__(self):
         return self
 
 
 class Newsletter(models.Model):
-    email = models.CharField(max_length=30, null=True, blank=True)
-        
+    email = models.CharField(max_length=30, null=True, blank=False)
+
     def __str__(self):
         return self
