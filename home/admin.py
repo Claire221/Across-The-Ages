cfe5674f7d9fe4d04bcfc00bad9bb77c72f10837
Claire221 <c.biglands@hotmail.com>
2023-02-17 11:19:37 +0000

    Add models to Admin dashboard

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact, Newsletter
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('newsletter_email',)
