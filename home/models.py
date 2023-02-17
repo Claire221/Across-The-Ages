5bb0dda69695505c74d40321ae357cab2d9501dc
Claire221 <c.biglands@hotmail.com>
2023-02-17 11:19:54 +0000

    Update contact and newsletter models

--- home/models.py
+++ home/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    first_name = models.CharField(max_length=20, null=True, blank=False)
+    first_name = models.CharField(max_length=20, null=False, blank=False)
     last_name = models.CharField(max_length=20, null=True, blank=True)
-    email = models.CharField(max_length=30, null=True, blank=False)
-    message = models.TextField(null=True, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
 
     def __str__(self):
         return self
 
 
 class Newsletter(models.Model):
-    newsletter_email = models.CharField(max_length=30, null=True, blank=False)
+    newsletter_email = models.EmailField(null=False, blank=False)
 
     def __str__(self):
         return self
